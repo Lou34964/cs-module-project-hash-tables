ffa7acaa80b96554412d39937bec096462b93f5f
Chad <Mapletonkid@gmail.com>
2020-06-04 13:20:13 -0600

    No_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
     # Your code here
 
+    word = ""
+
+    if len(s):
+        for w in s.split():
+            if w not in word:
+                if len(word):
+                    word += " "
+                word += w
+    return word
+
 
 
 if __name__ == "__main__":
